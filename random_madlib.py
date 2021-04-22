9d3e393e3f7bfb832d7fc2fd44101d1f9bc39d05
TonZaga <anthony.branham1@gmail.com>
2021-04-22 12:59:47 -0400

    Added stock madlibs and random generator

---
+++ random_madlib.py
@@ -0,0 +1,6 @@
+from stock_madlibs import letter_from_george as lfg
+import random
+
+if __name__ == "__main__":
+    m = random.choice([lfg])
+    m.madlib()
---
+++ stock_madlibs/letter_from_george.py
@@ -0,0 +1,23 @@
+def madlib():
+    p_noun = input("Plural noun: ")
+    occupation = input("Occupation: ")
+    place = input("Place: ")
+    number = input("Number: ")
+    adj = input("Adjective: ")
+    verb_ing = input("Verb ending in 'ing': ")
+    p_noun2 = input("Plural noun: ")
+    place2 = input("Place: ")
+    adj2 = input("Adjective: ")
+    p_noun3 = input("Plural noun: ")
+    verb_ing2 = input("Verb ending in 'ing': ")
+    p_noun4 = input("Plural noun: ")
+    adj3 = input("Adjective: ")
+    noun = input("Noun: ")
+    p_body = input("Part of the body: ")
+    verb = input("Verb: ")
+    adj4 = input("Adjective: ")
+    p_body2 = input("Part of the body: ") 
+
+    madlib = f"Hello, my fellow {p_noun} in 2020, it's me, George Washington, the first {occupation}.  I am writing from (the) {place}, where I have been secretly living for the past {number} years. I am concerned by the {adj} state of affairs in America these days.  It seems that your politicans are more concerned with {verb_ing} one another than with listening to the {p_noun2} of the people.  When we declared our independence from (the) {place2}, we set forth a/an {adj2} path guided by the voices of the everyday {p_noun3}.  If we're going to keep {verb_ing2}, then we need to learn how to respect all {p_noun4}.  Don't get me wrong, we had {adj3} problems in my day, too.  Benjamin Franklin once called me a/an {noun} and kicked me in the {p_body}.  But at the end of the day, we were able to {verb} in harmony.  Let us find that {adj4} spirit once again, or else I'm taking my {p_body2} off of the quarter!"
+
+    print(madlib)
